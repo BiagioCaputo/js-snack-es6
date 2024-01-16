d5a9ca108290fded228a6aae0f8771d4d57b6426
BiagioCaputo <scebiagio98@gmail.com>
2024-01-16 16:45:38 +0100

    Terminato snack 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-console.log("JS OK")
+/*************************************** SNACK 1 ********************************************/
+
+const guests = [
+  'Brad Pitt',
+  'Johnny Depp',
+  'Lady Gaga',
+  'Cristiano Ronaldo',
+  'Georgina Rodriguez',
+  'Chiara Ferragni',
+  'Fedez',
+  'George Clooney',
+  'Amal Clooney',
+  'Maneskin'
+];
+
+console.log(guests);
+
+const guestList = guests.map((guest, index) =>{
+    return {name: guest, tableName: 'Tavolo Vip', place: ++index};
+
+});
+
+console.log(guestList);
+
+/*************************************** SNACK 2 ********************************************/
+
+
