5036d9fee819b22366720e8fe783d7d9e3bc2a49
BiagioCaputo <scebiagio98@gmail.com>
2024-01-16 17:07:26 +0100

    Primi 2 punti dello snack 2

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ const guests = [
   'Lady Gaga',
   'Cristiano Ronaldo',
   'Georgina Rodriguez',
-  'Chiara Ferragni',
-  'Fedez',
+  'Ladra Ferragni ridicola',
+  'Fedez u mariuolo rapper fallito',
   'George Clooney',
   'Amal Clooney',
   'Maneskin'
@@ -24,4 +24,26 @@ console.log(guestList);
 
 /*************************************** SNACK 2 ********************************************/
 
+const students = [
+    {id: 213 , Name:'Marco della Rovere', grade: 78},
+    {id: 110 , Name:'Paola Cortellessa', grade: 96},
+    {id: 250 , Name:'Andrea Mantegna', grade: 48},
+    {id: 145 , Name:'Gaia Borromini', grade: 74},
+    {id: 196 , Name:'Luigi Grimaldello', grade: 68},
+    {id: 102 , Name:'Piero della Francesca', grade: 50},
+    {id: 120 , Name:'Francesca da Polenta', grade: 84},
+]
 
+console.log(students);
+
+//studenti con voti superiore a 70
+
+const goodGradeStudents = students.filter( student => student.grade > 70);
+
+console.log(goodGradeStudents);
+
+//studenti con voti superiore a 70 e id superiore a 120
+
+const goodGradeHighIdStudents = students.filter( student => student.grade > 70 && student.id > 120);
+
+console.log(goodGradeHighIdStudents);
